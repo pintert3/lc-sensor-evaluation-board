24c3cc2e6de9d993834a4f20123e91c99c135d35
mpiima <mpiima_mpiima@protonmail.com>
2021-07-05 12:58:59 +0300

    remove nova initial code

--- Sensorread/Sensorread.ino
+++ Sensorread/Sensorread.ino
@@ -2,7 +2,6 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include<SPI.h>
-#include <NovaSDS011.h>
 #include "Adafruit_SHT31.h"
 #include "ClosedCube_HDC1080.h"
 #include "Adafruit_HTU21DF.h"
@@ -10,7 +9,6 @@
 #define SDS_PIN_RX 2
 #define SDS_PIN_TX 3
 
-NovaSDS011 sds011;
 
 Adafruit_BME280 bme[3];
 
@@ -174,43 +172,7 @@ void setup() {
   // other sensor on channel 3 initialized here
   */
 
-  //-- nova setup---
   
-  sds011.begin(SDS_PIN_RX, SDS_PIN_TX);
-  /*
-  if (sds011.setWorkingMode(WorkingMode::work))
-  {
-    //Serial.println("SDS011 working mode \"Work\"");
-  }
-  else
-  {
-    Serial.println("FAIL: Unable to set working mode \"Work\"");
-    while (1) delay(10);//do nothing more
-  }
-
-  SDS011Version version = sds011.getVersionDate();
-
-  if (version.valid)
-  {
-    //Serial.println("SDS011 Firmware Vesion:\nYear: " + String(version.year) + "\nMonth: " +
-    //               String(version.month) + "\nDay: " + String(version.day));
-  }
-  else
-  {
-    Serial.println("FAIL: Unable to obtain Software Version");
-    while (1) delay(10);//do nothing more
-  }
-
-  if (sds011.setDutyCycle(5))
-  {
-    Serial.println("SDS011 Duty Cycle set to 5min");
-    
-  }
-  else
-  {
-    Serial.println("FAIL: Unable to set Duty Cycle");
-    while (1) delay(10);//do nothing more
-  }*/
 }
 
 void loop() {
@@ -230,14 +192,6 @@ void loop() {
   Serial.print("STM_mois. % = "); Serial.println(SMTmois);
   Readdata(0);
 
-  float p25, p10;
-  sds011.queryData(p25, p10) ;
-  
-    //Serial.println("Nova SDS011");
-    //Serial.println(String(millis() / 1000) + "s:PM2.5=" + String(p25) + ", PM10=" + String(p10));
-    Serial.println(String("Nova SDS011 ") + "PM2.5 = " + String(p25) + ", PM10 = " + String(p10));
-    Serial.println("-------------------------------------------------------------");
-    delay(6000);
   
   //Readdata(1);
   //Readdata(2);
