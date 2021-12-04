fbb9a3036e3756d7ccdca61449f0c9575548291c
Phillip Samuel K <phillipsamuelk3@gmail.com>
2021-12-04 10:49:51 +0300

    Add changes to full test to combined

--- combined/combined.ino
+++ combined/combined.ino
@@ -54,7 +54,7 @@ uint8_t OLD_DATA_AVAILABLE = 1;
 //tiny sensor objects
 Adafruit_BME280 bme[3];
 Adafruit_SHT31 sht31[3];
-HDC1080 hdc1080[2];
+HDC1080 hdc1080[3];
 Adafruit_HTU21DF htu[3];
 
 //particulate matter objects
@@ -194,22 +194,22 @@ void readData(int i,StaticJsonDocument<1024>& doc){
   }
 
   data = doc.createNestedArray(String("htu_")+String(i+1));
-    htu[i].readHumidity(); // just to make it work
-    data.add(htu[i].readTemperature());
-    delay(100);
-    data.add(htu[i].readHumidity());
-    delay(100);
+  htu[i].readHumidity(); // just to make it work
+  data.add(htu[i].readTemperature());
+  delay(100);
+  data.add(htu[i].readHumidity());
+  delay(100);
 
-  if (i<2){// we only have 2 of these
-    data = doc.createNestedArray(String("hdc_")+String(i+1));
-    Tcselect(i+4);
-    //We have to make sure they are connected otherwise since we have no status check for these yet.
-    // Their ".begin()" doesn't return anything
-    data.add(hdc1080[i].getTemperature());
-    delay(100);
-    data.add(hdc1080[i].getHumidity());
-    delay(100);
-  }
+  // if (i<2){// we only have 2 of these
+  data = doc.createNestedArray(String("hdc_")+String(i+1));
+  Tcselect(i+4);
+  //We have to make sure they are connected otherwise since we have no status check for these yet.
+  // Their ".begin()" doesn't return anything
+  data.add(hdc1080[i].getTemperature());
+  delay(100);
+  data.add(hdc1080[i].getHumidity());
+  delay(100);
+  // }
   wdt_disable();
 
   soft[i].listen();
@@ -298,12 +298,11 @@ void setup() {
     statusBME[i] =bme[i].begin(0x76); 
     delay(100);
     //Serial.println("before begin");
-    if(i<2){
-      Tcselect(i+4);
-      delay(100);
-      hdc1080[i].begin();// doesn't return boolean so just intialize 
-      delay(100);
-    }
+
+    Tcselect(i+4);
+    delay(100);
+    hdc1080[i].begin();// doesn't return boolean so just intialize 
+    delay(100);
   }
   wdt_disable();
   for(int i=0;i<3;i++){
